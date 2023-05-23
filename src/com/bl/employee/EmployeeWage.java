6f53cd7da6c920d67fac92ee31ecd2f2ab015db1
julinbestina <julin.bestina@yahoo.com>
2023-05-23 21:06:14 +0530

    Added:Code to calculate Wage

--- src/com/bl/employee/EmployeeWage.java
+++ src/com/bl/employee/EmployeeWage.java
@@ -3,12 +3,15 @@ package com.bl.employee;
 public class EmployeeWage {
     static final int FULL_TIME = 1;
     public static void main(String[] args) {
+        int empHour = 0;
 
         int empCheck = (int)(Math.random() *10)%2;
 
         if(empCheck == FULL_TIME)
-            System.out.println("Employee is present");
+           empHour = 8;
         else
-            System.out.println("Employee is absent");
+            empHour = 0;
+
+        System.out.println(empHour*20);
     }
 }
