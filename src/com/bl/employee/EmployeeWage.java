74f539fdb76824f19f7dc5ff2945f088f46fb2ba
julinbestina <julin.bestina@yahoo.com>
2023-05-23 21:01:37 +0530

    Added:Code to check employee attendance

--- src/com/bl/employee/EmployeeWage.java
+++ src/com/bl/employee/EmployeeWage.java
@@ -1,7 +1,14 @@
 package com.bl.employee;
 
 public class EmployeeWage {
+    static final int FULL_TIME = 1;
     public static void main(String[] args) {
-        System.out.println("EmployeeWage Program");
+
+        int empCheck = (int)(Math.random() *10)%2;
+
+        if(empCheck == FULL_TIME)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is absent");
     }
 }
